2506a8cf81382bd080a8ee8b7b4b76485f64735a
Pedram Valiani <p.valiani@gmail.com>
2021-02-09 17:57:40 +0000

    adds all models completed

--- src/main/java/com/example/codeclan/filesandfolders/models/User.java
+++ src/main/java/com/example/codeclan/filesandfolders/models/User.java
@@ -2,14 +2,45 @@ package com.example.codeclan.filesandfolders.models;
 
 
 import javax.persistence.Entity;
+import java.util.List;
 
 @Entity(name="users")
 public class User {
 
+    private Long id;
+    private String name;
+    private List<Folder> folders;
+
+    public User(String name) {
+        this.name = name;
+        this.folders = folders;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
+
     public User(){
-
-
-
     }
 
 
