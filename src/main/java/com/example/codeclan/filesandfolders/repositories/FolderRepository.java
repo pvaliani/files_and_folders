2624640961a2d82cdf53344caec1ab1eda1f63a9
Pedram Valiani <p.valiani@gmail.com>
2021-02-09 17:33:05 +0000

    repositories set up

--- src/main/java/com/example/codeclan/filesandfolders/repositories/FolderRepository.java
+++ src/main/java/com/example/codeclan/filesandfolders/repositories/FolderRepository.java
@@ -1,4 +1,9 @@
 package com.example.codeclan.filesandfolders.repositories;
 
-public interface FolderRepository {
+import com.example.codeclan.filesandfolders.models.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FolderRepository extends JpaRepository<Folder, Long> {
 }
