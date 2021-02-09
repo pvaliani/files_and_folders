b6b8d84675716c0cb18274e262d6ca7c84195c83
Pedram Valiani <p.valiani@gmail.com>
2021-02-09 17:37:08 +0000

    adds autowire to components - setting up object instances

--- src/main/java/com/example/codeclan/filesandfolders/components/DataLoader.java
+++ src/main/java/com/example/codeclan/filesandfolders/components/DataLoader.java
@@ -1,4 +1,31 @@
 package com.example.codeclan.filesandfolders.components;
 
+
+import com.example.codeclan.filesandfolders.models.File;
+import com.example.codeclan.filesandfolders.repositories.FileRepository;
+import com.example.codeclan.filesandfolders.repositories.FolderRepository;
+import com.example.codeclan.filesandfolders.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DataLoader {
+
+//    instantiate each repository type here
+    
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    FileRepository fileRepository;
+
+    @Autowired
+    FolderRepository folderRepository;
+
+
+    public DataLoader(){
+
+    }
+
+
 }
