aadefda0fe877319294970760bf1540bb75ca658
Pedram Valiani <p.valiani@gmail.com>
2021-02-09 18:04:38 +0000

    added column info for user model

--- src/main/java/com/example/codeclan/filesandfolders/models/Folder.java
+++ src/main/java/com/example/codeclan/filesandfolders/models/Folder.java
@@ -48,6 +48,7 @@ public class Folder {
         this.user = user;
     }
 
+
     public Folder(){
 
     }
--- src/main/java/com/example/codeclan/filesandfolders/models/User.java
+++ src/main/java/com/example/codeclan/filesandfolders/models/User.java
@@ -1,14 +1,31 @@
 package com.example.codeclan.filesandfolders.models;
 
 
-import javax.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import java.util.List;
 
-@Entity(name="users")
+@Entity
+@Table(name="users")
 public class User {
 
+//    column generation for the newly created table for the user model
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
+
+
+    @Column(name="name")
     private String name;
+
+//  user has a one to many relationship with folders and we use jsonignore to
+//  stop the serialization/repetition of nesting json within the API
+
+    @JsonIgnoreProperties({"user"})
+    @OneToMany(mappedBy = "user")
     private List<Folder> folders;
 
     public User(String name) {
@@ -41,6 +58,11 @@ public class User {
     }
 
     public User(){
+    }
+
+//    user can have a folder added to them
+    public void addFolder(Folder folder){
+        this.folders.add(folder);
     }
 
 
