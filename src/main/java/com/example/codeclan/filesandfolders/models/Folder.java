30f18fcc46f51e0cbaf1ccbaa237b2e304af3221
Pedram Valiani <p.valiani@gmail.com>
2021-02-09 18:19:02 +0000

    models complete now adding routes

--- src/main/java/com/example/codeclan/filesandfolders/models/Folder.java
+++ src/main/java/com/example/codeclan/filesandfolders/models/Folder.java
@@ -1,14 +1,32 @@
 package com.example.codeclan.filesandfolders.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name="folders")
 public class Folder {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
+
+    @Column(name="title")
     private String title;
-    private List<File> files;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    @JsonIgnoreProperties({"folders"})
     private User user;
+
+    @JsonIgnoreProperties({"folder"})
+    @OneToMany(mappedBy = "folder")
+    private List<File> files;
 
     public Folder(String title, User user) {
         this.title = title;
@@ -46,6 +64,11 @@ public class Folder {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+//   can add files
+    public void addFile(File file){
+        this.files.add(file);
     }
 
 
