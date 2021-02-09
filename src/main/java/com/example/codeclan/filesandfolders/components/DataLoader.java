26962f0215fce80b9b897aaaf956110521af2c3f
Pedram Valiani <p.valiani@gmail.com>
2021-02-09 18:26:52 +0000

    completed not tested

--- src/main/java/com/example/codeclan/filesandfolders/components/DataLoader.java
+++ src/main/java/com/example/codeclan/filesandfolders/components/DataLoader.java
@@ -2,6 +2,8 @@ package com.example.codeclan.filesandfolders.components;
 
 
 import com.example.codeclan.filesandfolders.models.File;
+import com.example.codeclan.filesandfolders.models.Folder;
+import com.example.codeclan.filesandfolders.models.User;
 import com.example.codeclan.filesandfolders.repositories.FileRepository;
 import com.example.codeclan.filesandfolders.repositories.FolderRepository;
 import com.example.codeclan.filesandfolders.repositories.UserRepository;
@@ -23,8 +25,42 @@ public class DataLoader {
     @Autowired
     FolderRepository folderRepository;
 
-
+//Seeds for db
     public DataLoader(){
+
+        User pedzilla = new User("Pedram");
+        userRepository.save(pedzilla);
+
+        Folder codeclan = new Folder("Codeclan Stuff", pedzilla);
+        folderRepository.save(codeclan);
+
+        Folder screenshots = new Folder("Screenshots", pedzilla);
+        folderRepository.save(screenshots);
+
+        File homework = new File("homework", ".pdf", "30mb", codeclan);
+        fileRepository.save(homework);
+
+        File shoes = new File("Shoes", ".jpg", "50mb", screenshots);
+        fileRepository.save(shoes);
+
+        File cv = new File("CV", ".txt", "100mb", codeclan);
+        fileRepository.save(cv);
+
+        File halloween = new File("Halloween dress up", ".jpg", "200mb", screenshots);
+        fileRepository.save(halloween);
+
+        codeclan.addFile(homework);
+        codeclan.addFile(cv);
+        screenshots.addFile(shoes);
+        screenshots.addFile(halloween);
+
+        folderRepository.save(codeclan);
+        folderRepository.save(screenshots);
+
+        pedzilla.addFolder(codeclan);
+        pedzilla.addFolder(screenshots);
+
+        userRepository.save(pedzilla);
 
 
     }
--- src/main/java/com/example/codeclan/filesandfolders/controllers/UserController.java
+++ src/main/java/com/example/codeclan/filesandfolders/controllers/UserController.java
@@ -1,4 +1,35 @@
 package com.example.codeclan.filesandfolders.controllers;
 
+import com.example.codeclan.filesandfolders.models.User;
+import com.example.codeclan.filesandfolders.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(value = "/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/users/{id}")
+    public ResponseEntity getUser(@PathVariable Long id){
+        return new ResponseEntity(userRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/users")
+    public ResponseEntity<User> postUser(@RequestBody User user){
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
 }
